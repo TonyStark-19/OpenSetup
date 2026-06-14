947d05d913b5e8df8acbf17540b07b43cfb14d2b
Aditya chandel <adityachandel371@gmail.com>
2026-06-14 22:33:49 +0530

    feat: setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-export default function App() {
+// import react
+import React from "react";
+
+// import routing components
+import { Route, Routes } from "react-router-dom";
+
+// import pages
+import HomePage from "./pages/HomePage";
+
+// main routing app component
+export default function App(): React.JSX.Element {
   return (
-    <h1 className="text-5xl text-amber-600">Hello, World!</h1>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
